05a5c8e2b69331c90cda35d8e483e2772cf16e8e
Sara S Camilleri <ssancam391@g.educaand.es>
2024-01-08 18:02:01 +0100

    tablaMultiplicar

---
+++ Main.kt
@@ -0,0 +1,23 @@
+/**
+ * Solicita un número entero en un rango.
+ *
+ * @param min Int - valor mínimo
+ * @param max Int - valor máximo
+ *
+ * @return Int - número entero válido dentro del rango especificado
+ */
+fun pedirNum
+
+/**
+ * Realiza una pregunta para contestar con s/si ó n/no
+ *
+ * @param text String - Texto de la pregunta
+ *
+ * @return Boolean - true/false dependiendo de la respuesta válida a la pregunta
+ */
+fun pregunta
+
+fun main() {
+    //TODO: Solicitar la introducción de un número entre 1 y 100 y mostrar su table de multiplicar...
+    //Hasta que se responda negativamente a la pregunta "¿Desea generar otra tabla de multiplicación? (s/n)"
+}
